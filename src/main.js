52f2597846d99eef6a9fc566ef5597ded2228a00
Amanpreet Singh <apsdehal@gmail.com>
2016-11-29 14:40:57 -0500

    Coloring works now

--- src/main.js
+++ src/main.js
@@ -25,14 +25,29 @@ function createElements(data) {
 
   edges = edges.map(function (x) {
     x['weight'] = 1;
+    x['source'] += 1;
+    x['target'] += 1;
     x['id'] = x['source'].toString() + '_' + x['target'].toString();
 
     return {data: x};
   });
 
+  edgesReverse = edges.map(function (data) {
+    var x = {};
+    x['source'] = data.data['target'];
+    x['target'] = data.data['source'];
+    x['weight'] = 1;
+    x['id'] = x['source'].toString() + '_' + x['target'].toString();
+
+    return {data: x};
+  })
+
+  Array.prototype.push.apply(edges, edgesReverse);
+
   nodes = data['nodes'];
 
   nodes = nodes.map(function (x) {
+    x['id'] += 1;
     return {data: x};
   });
 
@@ -40,7 +55,8 @@ function createElements(data) {
 }
 
 function startCyto() {
-
+  var start = '1';
+  var final = '50';
   var cy = cytoscape({
     container: document.getElementById('container'),
     elements: {
@@ -52,24 +68,42 @@ function startCyto() {
       {
         selector: 'node',
         style: {
-          'background-color': 'red',
+          'background-color': '#0072BB',
           'label': 'data(id)',
-          'width': 10,
-          'height': 10,
-          'font-size': 8
+          'width': 25,
+          'height': 25,
+          'font-size': 25
         }
       },
       {
         selector: 'edge',
         style: {
-          'line-color': 'green',
-          'width': 'mapData(weight, 0, 100, 1, 70)',
+          'line-color': '#FF4C3B',
+          'width': 'mapData(weight, 3, 100, 1, 70)',
         }
       },
       {
-        selector: '.highlighted',
+        selector: '.current',
         style: {
-            'background-color': '#61bffc',
+            'background-color': '#00FF00',
+            'line-color': '#61bffc',
+            'transition-property': 'background-color, line-color',
+            'transition-duration': '0.5s'
+        }
+      },
+      {
+        selector: '.next',
+        style: {
+            'background-color': '#FF4C3B',
+            'line-color': '#61bffc',
+            'transition-property': 'background-color, line-color',
+            'transition-duration': '0.5s'
+        }
+      },
+      {
+        selector: '.destination',
+        style: {
+            'background-color': '#FFCC00',
             'line-color': '#61bffc',
             'transition-property': 'background-color, line-color',
             'transition-duration': '0.5s'
@@ -82,6 +116,48 @@ function startCyto() {
       fit: true,
       randomize: true
     }
-  })
+  });
 
+  var current = start;
+  cy.getElementById(final).addClass('destination');
+
+  var neighbors = colorNeighbors();
+  cy.getElementById(start).removeClass('next').addClass('current');
+
+
+  function colorNeighbors() {
+    var nodes = cy.getElementById(current);
+    var connectedEdges = nodes.connectedEdges();
+
+    var connectedNodes = connectedEdges.targets();
+
+    Array.prototype.push(connectedNodes, connectedEdges.sources());
+
+    connectedNodes.forEach(function (node) {
+      console.log(node.data('id'));
+      node.addClass('next');
+    })
+
+    return {
+      uncolor: function () {
+        connectedNodes.forEach(function (node) {
+          node.removeClass('next');
+        });
+      }
+    };
+  }
+
+  cy.on('tap', 'node', function () {
+    var nodes = this;
+    cy.getElementById(current).removeClass('current');
+    neighbors.uncolor();
+
+    this.removeClass('next');
+    current = this.data('id');
+
+    neighbors = colorNeighbors();
+    this.removeClass('next');
+    this.addClass('current');
+    console.log(current);
+  });
 }
