5be04b99bc89b26d0aa088543e4b29ca921ecdea
Simranjyot Singh <simranjyot1991@gmail.com>
2016-12-05 21:34:07 -0500

    added medium graph and renamed data to large_graph

--- generate-graph.py
+++ generate-graph.py
@@ -2,4 +2,9 @@ import networkx as nx
 from networkx.readwrite import json_graph
 import json
 
-print json.dumps(json_graph.node_link_data(nx.fast_gnp_random_graph(80,0.03)))
+#large
+#print json.dumps(json_graph.node_link_data(nx.fast_gnp_random_graph(80,0.03)))
+#small
+#print json.dumps(json_graph.node_link_data(nx.fast_gnp_random_graph(20,0.10)))
+#medium
+print json.dumps(json_graph.node_link_data(nx.fast_gnp_random_graph(40,0.05)))
